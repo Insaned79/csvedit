cb56ed02ca0c0a9a84aacca39a5b4e44836698c7
Ilya T <tolstenkoiv@gmail.com>
2018-02-14 22:20:31 +0300

    -

--- csveditor.py
+++ csveditor.py
@@ -1,10 +1,22 @@
 from pandas import read_csv
+import os
 
+print('Введите имя файла [test.csv]')
+filename=str(input())
+
+if filename=='':
+    filename='test.csv'
+
+if not os.path.exists(filename):
+    print('Файл не найден')
+    quit(2)
 csv = read_csv('test.csv', sep=',', header=0, low_memory=False)
+sz=str(len(csv))
+print('Загружено строк: '+sz)
+print('Первые пять: ')
+print(csv[:5])
 
-min = -1
-max = -1
 
-s= csv.iloc[100]
-print (str(s))
+
+
 
